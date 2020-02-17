3264e87e81fb9111c5f8a3a4eb1d372d842e463b
Andre Freeman <asf@mail.usf.edu>
2020-02-17 17:06:42 -0500

    added preventDefault to nav links

--- js/index.js
+++ js/index.js
@@ -17,25 +17,29 @@ window.addEventListener('resize', function () {
 let home = document.querySelectorAll('a')[0];
 
 home.addEventListener('mouseover', function () {
-    home.style.color = 'blue'
+    home.style.color = 'blue';
+    home.preventDefault();
 });
 
 let aboutus = document.querySelectorAll('a')[1];
 
 aboutus.addEventListener('mouseover', function () {
-    aboutus.style.color = 'yellow'
+    aboutus.style.color = 'yellow';
+    aboutus.preventDefault();
 });
 
 let blog = document.querySelectorAll('a')[2];
 
 blog.addEventListener('mouseover', function () {
-    blog.style.color = 'green'
+    blog.style.color = 'green';
+    blog.preventDefault();
 });
 
 let contact = document.querySelectorAll('a')[3];
 
 contact.addEventListener('mouseover', function () {
-    contact.style.color = 'red'
+    contact.style.color = 'red';
+    contant.preventDefault();
 });
 
 
