9ac33aeeb5f57428a0b1a832984123e9a6678715
Andre Freeman <asf@mail.usf.edu>
2020-02-17 12:22:04 -0500

    Added Mouseover Event To Nav Bar Turning Blue

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+//main-navigation
+
+let home = document.querySelectorAll('a')[0];
+
+home.addEventListener('mouseover', function(){
+    home.style.color = 'blue'});
+
+let aboutus = document.querySelectorAll('a')[1];
+
+aboutus.addEventListener('mouseover', function(){
+    aboutus.style.color = 'blue'});
+
+let blog = document.querySelectorAll('a')[2];
+
+blog.addEventListener('mouseover', function(){
+    blog.style.color = 'blue'});
+
+let contact = document.querySelectorAll('a')[3];
+
+contact.addEventListener('mouseover', function(){
+    contact.style.color = 'blue'});
+
+
+
+
+
+
+
